1e93826690814e9fb6bb6d76f215ab34addef8a3
xiwang wang <simon9211@126.com>
2020-03-25 18:48:59 +0800

    v

--- luoow.py
+++ luoow.py
@@ -1,65 +1,37 @@
 # -*- coding:utf-8 -*-
 import re
 import threading
-
-import urllib.request
-
 import random
-from random import choice
 from time import sleep
 
 from urllib.request import urlopen
 
 import pymongo
-import requests
-from bs4 import BeautifulSoup
 import ssl
+
+# import db
+# from db import db
+import lib
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
 # 数据库
 
-db = client.luoow
+database = client.luoow
 # vol表,没有自动创建
-vol_db = db.vols
-tag_db = db.tags
-col_db = db.cols
+vol_db = database.vols
+tag_db = database.tags
+# col_db = db.col
 
 
 dblist = client.list_database_names()
 
 print('simon')
 
-USER_AGENT = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
-    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
-    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
-    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
-]
-
-
 def get_home_page():
-    # jd_url = 'http://www.luoow.com/'
-    jd_url = 'http://www.luoow.com/101_200.html'
-    # jd_url = 'http://www.luoow.com/701_800.html'
-    # response = requests.get(jd_url1)
-    res = requests.get(jd_url, headers={'User-Agent': USER_AGENT[1]})
-    # print(res.text)
-    # html = urlopen(jd_url)
-    bsObj = BeautifulSoup(res.text, 'html5lib')
+    url = 'http://www.luoow.com/'
+    bsObj = lib.load_page(url)
     # 根据css样式表查找
     print(bsObj)
     vols = []
@@ -83,23 +55,9 @@ def get_home_page():
     t_tags.start()
 
 
-    # colList = bsObj.findAll("div", {"class": "thumbnail theborder"})
-    # cols = []
-    # for col in colList:
-    #     # print(col)
-    #     item = {}
-    #     a = col.findAll('a')
-    #     item['title'] = a[1].get_text()
-    #     item['href'] = a[1].get('href')
-    #     img = col.find('img')
-    #     item['src'] = img.get('src')
-    #     cols.append(item)
-    # t_cols = threading.Thread(target=save_cols_mongo, args=(cols,))
-    # t_cols.start()
-    # print(cols)
-
 # 创建一个线程锁
 lock = threading.Lock()
+
 
 def get_vol(vols):
     for i in range(0, 4):
@@ -107,6 +65,7 @@ def get_vol(vols):
         # 启动线程
         t.start()
     # get_vol_items(vols)
+
 
 def get_vol_items(vols):
     while vols:
@@ -130,10 +89,8 @@ def get_vol_items(vols):
 
         url += tp
         idx = random.randint(0, 10)
-        res = requests.get(url, headers={'User-Agent': USER_AGENT[idx]})
-        print(url)
-        # print(res.text)
-        bsObj = BeautifulSoup(res.text, 'html5lib')
+
+        bsObj = lib.load_page(url)
         print(bsObj)
         # sleep(3)
         colList = bsObj.findAll("div", {"class": "thumbnail theborder"})
@@ -145,7 +102,7 @@ def get_vol_items(vols):
             item['title'] = a[1].get_text()
             item['href'] = a[1].get('href')
             img = col.find('img')
-            item['src'] = img.get('src')
+            item['cover'] = img.get('src')
             cols.append(item)
         t_cols = threading.Thread(target=save_cols_mongo, args=(cols,))
         t_cols.start()
@@ -158,16 +115,37 @@ def save_vols_mongo(vols):
         # 插入mongo
         vol_db.save({'vol': vols[i], "_id": i}, )
 
+
 def save_tags_mongo(tags):
     for tag in tags:
         # 插入mongo
         tag_db.insert({'tag': tag})
 
+
 def save_cols_mongo(cols):
     for col in cols:
-        col_db.insert(col)
+        col['_id'] = hash(col['title'])
+        # col_db.save(col)
+        # new_col = db.add_col(title=col['title'], href=col['href'], cover=col['cover'])
+        # if new_col:
+        #     # 插入成功
+        #     print('col 插入成功')
+        # else:
+        #     print('------------ Vol%s: Add Failed! ----------' % col)
 
 
 
 
-get_home_page()
+
+
+
+# get_home_page()
+
+def get_vol_detail():
+    url = 'http://www.luoow.com/999/'
+    print(url)
+    bsObj = lib.load_page(url)
+    print(bsObj)
+
+
+get_vol_detail()
